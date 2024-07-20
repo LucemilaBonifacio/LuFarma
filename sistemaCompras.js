93ea289a297ef4f55095a2197ff1bf435d0f9b87
Leonardo Martins <usantyerleo@gmail.com>
2024-07-20 15:13:29 -0300

    pagamento lufarma

--- sistemaCompras.js
+++ sistemaCompras.js
@@ -8,51 +8,54 @@ let pagarNaEntrega;
 let dadosCartao;
 let codCartao;
 
-function espacamentoDuplo() {   
+function espacamentoDuplo() {
     console.log();
     console.log();
 }
 
+function solicitarDadosCartao() {
+    dadosCartao = prompt("Insira dados do cartão: ");
+    codCartao = prompt("Insira código de segurança: ");
+    espacamentoDuplo();
+    if (dadosCartao.length === 16) {
+        console.log("Pagamento aprovado \nProsseguindo para entrega.");
+    } else {
+        console.log("Dados inválidos, tente novamente.");
+    }
+}
+
 console.log("Como deseja pagar?");
 espacamentoDuplo();
-let opcao = Number(prompt(" 1 -> Débito \n 2 -> Crédito \n 3 -> Pix \n 4 -> Pagar na entrega \n \n "));
+console.log(" 1 -> Débito \n 2 -> Crédito \n 3 -> Pix \n 4 -> Pagar na entrega \n");
+espacamentoDuplo();
+
+let opcao = Number(prompt("Insira a opção desejada: "));
 espacamentoDuplo();
 
 switch (opcao) {
     case 1:
         debito = true;
-        dadosCartao = prompt("Insira dados do cartão: ");
-        codCartao = prompt("Insira código de segurança: ");
-        espacamentoDuplo();
-        if(dadosCartao.length === 16){
-            console.log("Pagamento aprovado \n Prosseguindo para entrega.")
-        }else{
-            console.log("Dados inválidos, tente novamente.")
-        }
+        solicitarDadosCartao();
         break;
     case 2:
         credito = true;
-        dadosCartao = prompt("Insira dados do cartão: ");
-        codCartao = prompt("Insira código de segurança: ");
-        espacamentoDuplo();
-        if(dadosCartao.length === 16){
-            console.log("Pagamento aprovado \nProsseguindo para entrega.")
-        }else{
-            console.log("Dados inválidos, tente novamente.")
-        }
+        solicitarDadosCartao();
         break;
     case 3:
         pix = true;
         console.log("Gerando QR code");
-        let chavePix = "chavePixExemplo";
-        let descricao = "Descrição do pagamento via Pix";
-        let conteudoQR = `Chave Pix: ${chavePix}\nDescrição: ${descricao}`;
+        let conteudoQR = "Pagamento aprovado";
         QRCode.generate(conteudoQR, { small: true });
-        console.log("Pagamento aprovado\nProsseguindo para entrega.")
+        espacamentoDuplo();
+        console.log("\nProsseguindo para entrega.");
+        espacamentoDuplo();
+        break;
     case 4:
-        pagarNaEntrega = true;  
-        console.log("Prosseguindo para entrega."); 
+        pagarNaEntrega = true;
+        console.log("Prosseguindo para entrega.");
         break;
     default:
         console.log("Opção inválida. Tente novamente.");
 }
+
+module.exports = sistemaCompras;
