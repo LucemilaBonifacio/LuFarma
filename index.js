7db65873ece94d1e938058b8422e48a40ff22bf7
Lucemila Bonifácio <103540532+LucemilaBonifacio@users.noreply.github.com>
2024-07-20 10:35:52 -0300

    Add lista medicamentos

---
+++ index.js
@@ -0,0 +1,18 @@
+let medicamentos = [
+    { nome: "Aspirina", preco: 10.99 },
+    { nome: "Ibuprofeno", preco: 12.50 },
+    { nome: "Paracetamol", preco: 8.99 },
+    { nome: "Dorflex", preco: 15.99 },
+    { nome: "Neosaldina", preco: 20.99 }
+  ];
+  
+  let cosmeticos = [
+    { nome: "Shampoo", preco: 25.99 },
+    { nome: "Condicionador", preco: 30.99 },
+    { nome: "Creme de rosto", preco: 40.99 },
+    { nome: "Base de maquiagem", preco: 35.99 },
+    { nome: "Lápis de olho", preco: 10.99 }
+  ];
+
+console.log(medicamentos[0].nome); 
+console.log(medicamentos[0].preco); 
