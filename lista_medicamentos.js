29f716504fb8cee4cbf2926e7d8432a50de68c03
Lucemila Bonifácio <103540532+LucemilaBonifacio@users.noreply.github.com>
2024-07-20 12:23:40 -0300

    Add lista medicamentos

--- lista_medicamentos.js
+++ lista_medicamentos.js
@@ -1,18 +1,24 @@
-let medicamentos = [
-    { nome: "Aspirina", preco: 10.99 },
-    { nome: "Ibuprofeno", preco: 12.50 },
-    { nome: "Paracetamol", preco: 8.99 },
-    { nome: "Dorflex", preco: 15.99 },
-    { nome: "Neosaldina", preco: 20.99 }
-  ];
-  
-  let cosmeticos = [
-    { nome: "Shampoo", preco: 25.99 },
-    { nome: "Condicionador", preco: 30.99 },
-    { nome: "Creme de rosto", preco: 40.99 },
-    { nome: "Base de maquiagem", preco: 35.99 },
-    { nome: "Lápis de olho", preco: 10.99 }
-  ];
-
-console.log(medicamentos[0].nome); 
-console.log(medicamentos[0].preco); 
+let listaMedicamentos = [
+  { nome: "Ibuprofeno", preco: 10.99 },
+  { nome: "Paracetamol", preco: 8.99 },
+  { nome: "Amoxicilina", preco: 25.99 },
+  { nome: "Dipirona", preco: 12.99 },
+  { nome: "Aspirina", preco: 6.99 },
+  { nome: "Ciprofloxacino", preco: 30.99 },
+  { nome: "Azitromicina", preco: 35.99 },
+  { nome: "Omeprazol", preco: 20.99 },
+  { nome: "Ranitidina", preco: 18.99 },
+  { nome: "Naproxeno", preco: 15.99 },
+  { nome: "Cetoprofeno", preco: 22.99 },
+  { nome: "Diclofenaco", preco: 28.99 },
+  { nome: "Fluoxetina", preco: 32.99 },
+  { nome: "Loratadina", preco: 19.99 },
+  { nome: "Metformina", preco: 24.99 },
+  { nome: "Sinvastatina", preco: 29.99 },
+  { nome: "Histamin", preco: 34.99 },
+  { nome: "Losartana", preco: 26.99 },
+  { nome: "Enalapril", preco: 21.99 },
+  { nome: "Amlodipina", preco: 27.99 },
+  { nome: "Valsartana", preco: 31.99 }
+];
+module.exports = listaMedicamentos;
