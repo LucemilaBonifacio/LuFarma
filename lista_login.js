b718dc0a6af3fda8a5a087f5751d833110ba4c6b
Emanuelly16 <165863187+Emanuelly16@users.noreply.github.com>
2024-07-20 11:25:03 -0300

    Add lista login

---
+++ lista_login.js
@@ -0,0 +1,21 @@
+let login = [
+    {nome: "Emanuelly", senha: 1234},
+    {nome: "Lucemila", senha: 1234},
+    {nome: "Lucas", senha: 1234},
+    {nome: "Maya", senha: 1234},
+    {nome:"Bruno", senha: 1234},
+    {nome:"Pedro", senha: 1234},
+    {nome:"Leonardo", senha: 1234},
+    {nome:"Luciano", senha: 1234},
+    {nome:"Sillas", senha: 1234},
+    {nome:"Mielle", senha: 1234}
+];
+console.log(login[0].nome);
+console.log(login[0].senha);
+
+
+    
+    
+    
+
+
